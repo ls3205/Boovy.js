bb3f33fb189f567056498c6b2ba54e2dd39f1b5d
ls3205 <ls3205@trevor.org>
2021-11-15 18:20:01 -0500

    Reformat

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,92 @@ import {
 import ytdl from 'ytdl-core';
 import * as yts from 'youtube-search-without-api-key'
 
+const client = new DiscordJS.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
+const player = createAudioPlayer();
+
+import dotenv from 'dotenv';
+dotenv.config();
+const token = process.env.TOKEN;
+const prefix = process.env.PREFIX;
+
+async function search(query) {
+    let json = await yts.search(query);
+    let url = await json[0].url;
+    await console.log("{Search2 Function} Link recieved is: " + url);
+    return url;
+}
+
+async function play(message, query) {
+    let url = await search(query);
+    console.log("{Play Function} Link recieved is: " + url);
+    const resource = createAudioResource(url);
+    message.reply(`Now playing, ${url}!`);
+}
+
+async function join(channel: VoiceChannel) {
+    const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+    
+    try {
+        await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+        return connection;
+    } catch (error) {
+        connection.destroy();
+        throw error;
+    }
+}
+
+client.once('ready', () => {
+    console.log(`Ready!, Logged in as ${client.user?.tag}`);
+});
+client.once('reconnecting', () => {
+    console.log('Reconnecting!');
+});
+client.once('disconnect', () => {
+    console.log('Disconnect!');
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(`${prefix}`)) return;
+    
+    const content = String(message);
+    const args = content.substr(content.indexOf(' ')+1);
+    console.log("Arguments recieved: " + args);
+    
+    if (message.content.startsWith(`${prefix}play`)) {
+        play(message, args);
+    }
+    if (message.content.startsWith(`${prefix}search`)) {
+        console.log(search(args));
+    }
+    if (message.content.startsWith(`${prefix}linktest`)) {
+        var linkurl = await search(args);
+        message.reply(`Link recieved: ${linkurl}`);
+    }
+    // if (message.content.startsWith(`${prefix}join`)) {
+    //     const channel = message.member?.voice.channel;
+        
+    //     if (channel) {
+    //         try {
+    //             const connection = await join(channel);
+    //             console.log(connection);
+    //             connection.subscribe(player);
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     } else {
+    //         void message.reply('Join a voice channel then try again!');
+    //     }
+    // }
+});
+                    
+client.login(token);
+                    
+/*
 async function search(query) {
     var link;
     const makeRequest = new Promise<any>((resolve, reject) => {
@@ -41,98 +127,4 @@ async function search(query) {
     });
     return link;
 }
-
-async function search2(query) {
-    let json = await yts.search(query).then(function (value) {
-        return value;
-    });
-    let url = await json[0].url;
-    await console.log("{Search2 Function} Link recieved is: " + url);
-    return url;
-}
-
-const client = new DiscordJS.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
-
-import dotenv from 'dotenv';
-import { channel } from 'diagnostics_channel';
-import { link } from 'fs';
-dotenv.config();
-const token = process.env.TOKEN;
-const prefix = process.env.PREFIX;
-
-const player = createAudioPlayer();
-
-async function play(message, query) {
-    let url = await search2(query);
-    console.log("{Play Function} Link recieved is: " + url);
-    const resource = createAudioResource(url);
-    message.reply(`Now playing, ${url}!`);
-}
-
-function highest_search(query) {
-    let video = yts.search(query);
-    return video;
-}
-
-async function join(channel: VoiceChannel) {
-    const connection = joinVoiceChannel({
-        channelId: channel.id,
-        guildId: channel.guild.id,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-    });
-
-    try {
-        await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
-        return connection;
-    } catch (error) {
-        connection.destroy();
-        throw error;
-    }
-}
-
-client.once('ready', () => {
-    console.log(`Ready!, Logged in as ${client.user?.tag}`);
-});
-client.once('reconnecting', () => {
-    console.log('Reconnecting!');
-});
-client.once('disconnect', () => {
-    console.log('Disconnect!');
-});
-
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(`${prefix}`)) return;
-
-    const content = String(message);
-    const args = content.substr(content.indexOf(' ')+1);
-    console.log("Arguments recieved: " + args);
-
-    if (message.content.startsWith(`${prefix}play`)) {
-        play(message, args);
-    }
-    if (message.content.startsWith(`${prefix}search`)) {
-        console.log(search(args));
-    }
-    if (message.content.startsWith(`${prefix}linktest`)) {
-        var linkurl = await search2(args);
-        message.reply(`Link recieved: ${linkurl}`);
-    }
-    // if (message.content.startsWith(`${prefix}join`)) {
-    //     const channel = message.member?.voice.channel;
-
-    //     if (channel) {
-    //         try {
-    //             const connection = await join(channel);
-    //             console.log(connection);
-    //             connection.subscribe(player);
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     } else {
-    //         void message.reply('Join a voice channel then try again!');
-    //     }
-    // }
-});
-
-client.login(token);
+*/
