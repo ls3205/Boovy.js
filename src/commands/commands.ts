3020931693cd749a3d00f77d494bea16dce71335
ls3205 <ls3205@trevor.org>
2021-11-15 22:42:44 -0500

    Moved commands to different file Will add comments later

---
+++ src/commands/commands.ts
@@ -0,0 +1,51 @@
+import DiscordJS, { Message, VoiceChannel } from 'discord.js';
+import {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    entersState,
+    StreamType,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+    DiscordGatewayAdapterCreator,
+} from '@discordjs/voice';
+import * as yts from 'youtube-search-without-api-key'
+import * as DisTube from 'distube';
+const client = new DiscordJS.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_VOICE_STATES'] });
+const dst = new DisTube.default(client);
+
+export async function search(query) {
+    let json = await yts.search(query);
+    let url = await json[0].url;
+    await console.log("{Search2 Function} Link recieved is: " + url);
+    return url;
+}
+
+export async function play(message, query) {
+    let url = await search(query);
+    dst.play(message, url)
+};
+
+export async function skip(message, query) {
+    dst.skip(message);
+}
+
+export async function join(message) {
+    const connection = joinVoiceChannel({
+        channelId: message.member?.voice.channel?.id,
+        guildId: message.guild?.id,
+        adapterCreator: message.guild?.voiceAdapterCreator as unknown as DiscordGatewayAdapterCreator
+    });
+
+    try {
+        await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+        return connection;
+    } catch (error) {
+        connection.destroy();
+        throw error;
+    }
+}
+
+export async function stop(message) {
+    dst.stop(message);
+}
--- src/index.ts
+++ src/index.ts
@@ -11,43 +11,15 @@ import {
 } from '@discordjs/voice';
 import * as yts from 'youtube-search-without-api-key'
 import * as DisTube from 'distube';
+import * as commands from './commands/commands';
 
 const client = new DiscordJS.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_VOICE_STATES'] });
-const dst = new DisTube.default(client);
-const queue = new Map()
+export const dst = new DisTube.default(client);
 
 import dotenv from 'dotenv';
 dotenv.config();
 const token = process.env.TOKEN;
 const prefix = process.env.PREFIX;
-
-async function search(query) {
-    let json = await yts.search(query);
-    let url = await json[0].url;
-    await console.log("{Search2 Function} Link recieved is: " + url);
-    return url;
-}
-
-async function play(message, query) {
-    let url = await search(query);
-    dst.play(message, url)
-};
-
-async function join(message) {
-    const connection = joinVoiceChannel({
-        channelId: message.member?.voice.channel?.id,
-        guildId: message.guild?.id,
-        adapterCreator: message.guild?.voiceAdapterCreator as unknown as DiscordGatewayAdapterCreator
-    });
-
-    try {
-        await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
-        return connection;
-    } catch (error) {
-        connection.destroy();
-        throw error;
-    }
-}
 
 client.once('ready', () => {
     console.log(`Ready!, Logged in as ${client.user?.tag}`);
@@ -66,13 +38,15 @@ client.on('messageCreate', async (message) => {
     const content = String(message);
     const args = content.substr(content.indexOf(' ') + 1);
     console.log("Arguments recieved: " + args);
-    const serverQueue = queue.get(message.guild?.id);
 
     if (message.content.startsWith(`${prefix}play`)) {
-        play(message, args);
+        commands.play(message, args);
     }
     if (message.content.startsWith(`${prefix}join`)) {
-        join(message);
+        commands.join(message);
+    }
+    if (message.content.startsWith(`${prefix}stop`)) {
+        commands.stop(message);
     }
 });
 
